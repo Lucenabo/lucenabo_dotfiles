c62d6b275ef411c4035a0e1e8986d06721485460
lucenabo <alucena1002@gmail.com>
2024-05-23 21:46:26 +0200

    unzip

---
+++ bin/htb_target.sh
@@ -0,0 +1,14 @@
+[bar/micomponente]
+inherit = bar/main
+width = 16%
+height = 40
+offset-x = 80.5%
+offset-y = 15
+background = ${color.bg}
+foreground = ${color.white}
+bottom = false
+padding = 1
+module-margin-left = 0
+module-margin-right = 0
+modules-center = target_to_hack
+wm-restack = bspwm
